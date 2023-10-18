d7c547a4756a5fef7006e4b3c6073a1e4d60221a
LeThanhDat <Le Thanh Dat>
2023-10-18 15:55:25 +0700

    temporary send message to room by add a connection to params

--- app/app.js
+++ app/app.js
@@ -8,18 +8,49 @@ socket.on('message', text => {
     document.querySelector('ul').appendChild(el)
 
 });
-document.querySelector('input').focus()
+
+socket.on('messagePrivate', ({text,room}) => {
+
+    const el = document.createElement('li');
+    el.innerHTML = `${room}: ${text}`;
+    document.querySelector('ul').appendChild(el)
+
+});
+
+document.querySelector('#message').focus()
 
 const sendMessage = ()=>{
-    let text = document.querySelector('input').value;
+    let text = document.querySelector('#message').value;
     if(text) socket.emit('message', text)
-    document.querySelector('input').value = ''
+    document.querySelector('#message').value = ''
 }
 
-document.querySelector('button').onclick = sendMessage
+const sendMessageRoom = ()=>{
+    const room = document.querySelector('#room') ? document.querySelector('#room').value : ''
+    let message = document.querySelector('#message').value;
+    if(message) socket.emit('messagePrivate', {message,room})
+    document.querySelector('#message').value = ''
+}
+
+
+const joinRoom = () =>{
+    const room = document.querySelector('#room') ? document.querySelector('#room').value : ''
+    console.log(room)
+    socket.emit("join-room",{room,message:'join new room message'})
+    // return document.querySelector('#room').value
+
+    // socket.joinRoom(e.target.value)
+}
+
+document.querySelector('#join').onclick = joinRoom
+
+document.querySelector('#send').onclick = sendMessageRoom
 window.addEventListener('keypress',(e)=>{
-    if (e.key === 'Enter') sendMessage()
+    const room = document.querySelector('#room') ? document.querySelector('#room').value : ''
+    if (e.key === 'Enter' && room) sendMessage()
 })
+
+
 // Regular Websockets
 
 // const socket = new WebSocket('ws://localhost:8080');
--- server/index.js
+++ server/index.js
@@ -1,28 +1,36 @@
+const http = require("http").createServer();
 
-const http = require('http').createServer();
-
-const io = require('socket.io')(http, {
-    cors: { origin: "*" }
+const io = require("socket.io")(http, {
+  cors: { origin: "*" },
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-
-    socket.on('message', (message) =>     {
-        console.log(message);
-        io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
-    });
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("message", (message) => {
+    console.log(message)
+    // io.emit("message", `${socket.id.substr(0, 2)} said ${message}`);
+    io.emit("message",`${socket.id.substr(0, 2)} said ${message} `)
+});
+  socket.on("join-room",({message,room})=>{
+    // console.log(room)
+    io.emit("message",`welcome to room: ${room}, message is: ${message}`)
+  })
+  socket.on("messagePrivate", ({message,room}) => {
+    console.log({message,room})
+    // io.emit("message", `${socket.id.substr(0, 2)} said ${message}`);
+    io.to(room).emit("message",`room ${room}: ${socket.id.substr(0, 2)} said ${message} `)
+});
 });
 
-http.listen(8080, () => console.log('listening on http://localhost:8080') );
 
+http.listen(8080, () => console.log("listening on http://localhost:8080"));
 
 // Regular Websockets
 
 // const WebSocket = require('ws')
 // const server = new WebSocket.Server({ port: '8080' })
 
-// server.on('connection', socket => { 
+// server.on('connection', socket => {
 
 //   socket.on('message', message => {
 
@@ -31,6 +39,3 @@ http.listen(8080, () => console.log('listening on http://localhost:8080') );
 //   });
 
 // });
-
-
- 
